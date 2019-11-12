9e1bea0b758ab88f1bb5c983bcccf1c4c2e6b3fa
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-11-12 17:03:34 +0100

    Create addImage function to be the callback for xhr Image request

--- js/app.js
+++ js/app.js
@@ -11,7 +11,26 @@
 		resultContainer.innerHTML = '';
 		let searchText = searchInput.value;
 		
-		
+
+		// Add Image function for add the image to results container
+		function addImage(){
+			let htmlContent = '';
+			data = JSON.parse(this.responseText);
+			console.log(data.results);
+
+			if(data && data.results && data.results[1]){
+				const firstImage = data.results[1];
+				htmlContent = `<figure>
+				<img src="${firstImage.urls.regular}" alt=${searchText}>
+				<figcaption>${searchText} by ${firstImage.user.name} </figcaption>
+				</figure>
+				`
+			}else{
+				htmlContent = `<div class="error-no-image">No Image available</div>`
+			}
+			resultContainer.insertAdjacentHTML('afterbegin', htmlContent);
+		}
 	});
+
 
 })();
