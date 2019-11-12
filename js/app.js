f1fa364805c6f6bb36974a91a1b86f6e31f42bb5
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2019-11-12 16:38:46 +0100

    Add EventListener to the form

--- js/app.js
+++ js/app.js
@@ -4,7 +4,14 @@
 	const submitBtn = document.querySelector('#submit-btn');
 
 	const resultContainer = document.querySelector('#search-results');
-	
 
+	
+	searchForm.addEventListener('submit', function(evt){
+		evt.preventDefault();
+		resultContainer.innerHTML = '';
+		let searchText = searchInput.value;
+		
+		
+	});
 
 })();
